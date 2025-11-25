bee0966a2d7a74ce0c494044a300eb2eff30051b
marymo <mo.mary.gabin@gmail.com>
2025-11-25 09:45:29 -0700

    added dev buttons

--- Station/View/ActionSelectionScreen/action_selection_screen.py
+++ Station/View/ActionSelectionScreen/action_selection_screen.py
@@ -3,4 +3,14 @@ from kivy.app import App
 from kivy.clock import Clock
 
 class ActionSelectionScreen(MDScreen):
-    pass
+    def go_to_main(self):
+        """Navigate to the Welcome screen."""
+        self.manager.current = 'welcome screen'
+    
+    def borrow_tool(self):
+        """persist borrow state"""
+        print("BORROWING")
+    
+    def return_tool(self):
+        """persist return state"""
+        print("RETURNING")
