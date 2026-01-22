2104e61ca12a81aed3bb56aa61480cf01f0aeca5
Yopdiff <hongwoo.yoon@ucalgary.ca>
2026-01-22 12:08:34 -0500

    EDIT: same network allow

--- AdminWeb/vite.config.ts
+++ AdminWeb/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allows access from network (e.g. laptop)
+  }
 })
