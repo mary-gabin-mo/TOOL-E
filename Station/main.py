dbaf7f6e6b892f508105f612d962581bdd880760
marymo <mo.mary.gabin@gmail.com>
2026-03-18 14:01:44 -0600

    PI: fixing touch orientation

--- Station/main.py
+++ Station/main.py
@@ -35,7 +35,7 @@ if IS_RASPBERRY_PI:
     Config.set('input', 'mouse', 'mouse, disable_multitouch')
 
     # Fix the "random spot" bug (Matches touch rotation to screen rotation)
-    Config.set('graphics', 'rotation', '0')
+    Config.set('graphics', 'rotation', '270')
     
 else:
     print("System: Dev Environment detected. Setting WINDOWED.")
